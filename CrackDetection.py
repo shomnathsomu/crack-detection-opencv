d01f3b756896718ba59fc94506ce39d9c0de1068
shomnath pramanik <shomnathcse22@gmail.com>
2020-05-03 22:03:21 +0600

    Apply logarithmic transform

--- CrackDetection.py
+++ CrackDetection.py
@@ -11,3 +11,6 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Inverting / negative image
 imgNeg = cv2.bitwise_not(gray)
+
+# Apply logarithmic transform
+img_log = (np.log(imgNeg+1)/(np.log(1+np.max(imgNeg))))*255
