9dd7688cfff59f3ea388a966fb983e7a3c90276c
shomnath pramanik <shomnathcse22@gmail.com>
2020-05-03 22:02:16 +0600

    Convert into gray scale

--- CrackDetection.py
+++ CrackDetection.py
@@ -5,3 +5,6 @@ from matplotlib import pyplot as plt
 
 # read a cracked sample image
 img = cv2.imread('samples/query_06.bmp')
+
+# Convert into gray scale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
